93690a9eeba1ab759c6d272c801086b119065d7e
Christa <82023874+christa-ux@users.noreply.github.com>
2025-07-24 16:20:09 -0400

    Create Customer.java

---
+++ Models/Customer.java
@@ -0,0 +1,39 @@
+package Models;
+public class Customer extends User {
+	protected String nationality;
+	protected String dateOfBirth;
+	private String passportNumber;
+	
+	public Customer (int id, String name, String email, String phoneNumber, String username, String password, String role, String nationality, String dateOfBirth, String passportNumber)  {
+		super (id, name, email, phoneNumber, username, password, "customer" );
+		this.nationality=nationality;
+		this.dateOfBirth=dateOfBirth;
+		this.passportNumber=passportNumber;
+	}
+	
+	public String getNationality() {
+		return nationality;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public String getPassportNumber() {
+		return passportNumber;
+	}
+	 public void setNationality (String nationality) {
+		 this.nationality=nationality;
+	 }
+	 public void setDateOfBirth(String dateOfBirth) {
+		 this.dateOfBirth=dateOfBirth;
+	 }
+	 public void setPassportNumber(String passportNumber) {
+		 this.passportNumber=passportNumber;
+	 }
+	
+	@Override
+	public String toString() {
+		return super.toString() +  " | DOB: " + dateOfBirth +
+	               " | Nationality: " + nationality +
+	               " | Passport: " + passportNumber;
+	}
+}
