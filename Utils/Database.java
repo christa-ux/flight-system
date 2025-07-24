0d3af795f5360f34c66432b1100ec3aed49de8ac
Christa <82023874+christa-ux@users.noreply.github.com>
2025-07-24 16:51:42 -0400

    Update Database.java

--- Utils/Database.java
+++ Utils/Database.java
@@ -1,1 +1,19 @@
+package Utils;
+import Models.*;
+import java.util.*;
 
+
+public class Database {
+public static List <User> users =new ArrayList<>();
+//public static List <Flight> flights =new ArrayList<>();
+//public static List<Booking> bookings = new ArrayList<>();
+//public static List<Airline> airlines = new ArrayList<>();
+//public static List<Airport> airports = new ArrayList<>();
+
+
+public static int userCount=0;
+public static int flightCount=0;
+public static int bookingCount=0;
+public static int airlineCount=0;
+public static int airportCount=0;
+}
